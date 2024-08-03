60e9f932f2ce384dfd14850890052733762df2b1
Amarjeet <amarjeetmistri41@gmail.com>
2024-08-03 23:17:38 +0530

    add files

---
+++ college-management-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminDashboard from "./components/AdminDashboard";
+import "./App.css"; // Optional: Import CSS for styling
+
+
+function App() {
+  return (
+    <div className="App">
+      <AdminDashboard />
+    </div>
+  );
+}
+
+export default App;
