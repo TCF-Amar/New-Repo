6269b1cd56eef81364000d1b89e4e41acb0b56c9
Amarjeet <amarjeetmistri41@gmail.com>
2024-08-05 22:37:13 +0530

    add files

---
+++ college-management-frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <div className="container">
+
+            <h1 className=' w-full bg-gray-400'>Dashboard</h1>
+            </div>
+        </React.Fragment>
+    )
+    
+}
+export default Home; 
